7e52f7e760232402a60d1e081309fb8f63993ab5
Samuelms <samuel.sarimo@gmail.com>
2025-04-24 11:36:08 +0300

    Add eslint & prettier to backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,14 @@
-import express from "express";
-import api from "./api/index.js";
-import cors from "cors";
+import express from 'express';
+import api from './api/index.js';
+import cors from 'cors';
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Welcome to my REST API!");
+app.get('/', (req, res) => {
+  res.send('Welcome to my REST API!');
 });
 
-app.use("/api/v1", api);
+app.use('/api/v1', api);
 
 export default app;
