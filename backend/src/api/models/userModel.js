0c2e04ad8d1fd16b69a0b9a95918a03d3ce1593b
Samuelms <samuel.sarimo@gmail.com>
2025-04-25 14:55:24 +0300

    fix register route to not have type parameter

--- backend/src/api/models/userModel.js
+++ backend/src/api/models/userModel.js
@@ -1,4 +1,4 @@
-import promisePool from "../../utils/database.js";
+import promisePool from '../../utils/database.js';
 
 const addUser = async (user) => {
   const {
@@ -10,11 +10,10 @@ const addUser = async (user) => {
     address,
     city,
     postal_code,
-    type,
   } = user;
 
-  const sql = `INSERT INTO users (first_name, last_name, email, password, phone, address, city, postal_code, type)
-                   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  const sql = `INSERT INTO users (first_name, last_name, email, password, phone, address, city, postal_code)
+                   VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
 
   const params = [
     first_name,
@@ -25,21 +24,20 @@ const addUser = async (user) => {
     address,
     city,
     postal_code,
-    type,
   ];
 
   try {
     const [rows] = await promisePool.execute(sql, params);
-    console.log("Rows affected:", rows);
+    console.log('Rows affected:', rows);
 
     if (rows.affectedRows === 0) {
       return false;
     }
-    return { user_id: rows.insertId };
+    return {user_id: rows.insertId};
   } catch (error) {
-    console.error("Error inserting user:", error.message);
+    console.error('Error inserting user:', error.message);
     throw new Error(`Database error: ${error.message}`);
   }
 };
 
-export { addUser };
+export {addUser};
