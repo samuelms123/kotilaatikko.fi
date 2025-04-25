5a6e40aad287374b1078c026a6459a489dc6d8b1
Samuelms <samuel.sarimo@gmail.com>
2025-04-25 11:36:41 +0300

    Add backend login route, controller, model and rest file for testing the login

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,8 +1,10 @@
-import express from "express";
-import userRouter from "./routes/userRouter.js";
+import express from 'express';
+import userRouter from './routes/userRouter.js';
+import authRouter from './routes/authRouter.js';
 
 const router = express.Router();
 
-router.use("/users", userRouter);
+router.use('/users', userRouter);
+router.use('/auth', authRouter);
 
 export default router;
