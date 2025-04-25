4a6131be37f65ecddcfcd9aae06f2c26a25a543e
aruraruri <aurilait@gmail.com>
2025-04-25 09:46:28 +0300

    add login link to navigation bar

--- frontend/src/Components/NavigationBarDesktop.jsx
+++ frontend/src/Components/NavigationBarDesktop.jsx
@@ -13,6 +13,10 @@ const NavigationBarDesktop = () => {
           <Link className='hover:text-[var(--primary-color)]' to="/profile">Profiili</Link>
           <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-[var(--primary-color)] "></span>
         </li>
+        <li className='font-bold text-[1.5vh] group text-[var(--black-color)] transition duration-300'>
+          <Link className='hover:text-[var(--primary-color)]' to="/login">Kirjaudu sisään</Link>
+          <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-[var(--primary-color)] "></span>
+        </li>
       </ul>
     </nav>
   )
