13ec327613798cdb3540cf3a3e2e6e9d17805e56
beeboy98 <niko.mehilainen@gmail.com>
2025-04-29 12:19:55 +0300

    add shop-page + small mofications in layer and index.html

--- frontend/src/Components/Layout.jsx
+++ frontend/src/Components/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = () => {
     <div className='max-w-screen mx-auto'>
       <header className="border-b-2 border-[var(--primary-color)]">
         <div className='flex justify-between items-center p-4 max-w-[1600px] mx-auto'>
-          <h1 className='text-[3vh] font-[header]'>kotilaatikko.fi</h1>
+          <h1 className='text-3xl font-[header]'>kotilaatikko.fi</h1>
           <nav>
           <div className="hidden md:block">
             <NavigationBarDesktop/>
@@ -27,7 +27,7 @@ const Layout = () => {
 
       <footer className='bg-[var(--grey-color)] pb-12 text-[var(--white-color)]'>
         <div className="max-w-[1600px] mx-auto text-center">
-          <p className='py-10 text-[1.5vh]'>ASIAKASPALVELU</p>
+          <p className='py-10 text-lg font-[header]'>ASIAKASPALVELU</p>
 
           <p className='pt-5 font-bold'>Yhteystiedot</p>
           <p className='pt-3'>Puhelin: +358 40 66 420</p>
@@ -35,7 +35,7 @@ const Layout = () => {
           <p>Y-tunnus: 1234567-8</p>
 
           <div className='pt-15'>
-            <p>&#169; kotilaatikko.fi 2025 &#8212; Helposti ruokaa kotiovellesi!</p>
+            <p className='text-[90%]'>&#169; kotilaatikko.fi 2025 &#8212; Helposti ruokaa kotiovellesi!</p>
             <div className='flex justify-center pt-5'>
               <a href='http://tiktok.com'><img className='w-auto h-8 mr-2' src="/tt-icon.png" alt="TikTok brand logo." /></a>
               <a href='http://facebook.com'><img className='w-auto h-8 mr-2' src="/fb-icon.png" alt="Facebook brand logo." /></a>
