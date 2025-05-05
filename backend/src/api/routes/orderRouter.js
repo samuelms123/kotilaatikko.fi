759702315884081f4fc974c7d2d4b9d917ff3b29
Samuelms <samuel.sarimo@gmail.com>
2025-05-05 12:57:50 +0300

    add dummy order functionality

--- backend/src/api/routes/orderRouter.js
+++ backend/src/api/routes/orderRouter.js
@@ -1,13 +1,21 @@
 import express from 'express';
-import {handleGetOrders, handleGetOrderDetailsById, handlePostOrder, handleDeleteOrder} from '../controllers/orderController.js';
+import {
+  handleGetOrders,
+  handleGetOrderDetailsById,
+  handlePostOrder,
+  handleDeleteOrder,
+} from '../controllers/orderController.js';
 import {adminCheck, authenticateToken} from '../../middlewares.js';
 const orderRouter = express.Router();
 
 orderRouter.route('/').get(authenticateToken, adminCheck, handleGetOrders);
-orderRouter.route('/:id').get(authenticateToken, adminCheck, handleGetOrderDetailsById);
+orderRouter
+  .route('/:id')
+  .get(authenticateToken, adminCheck, handleGetOrderDetailsById);
 
-orderRouter.route('/').post(authenticateToken, adminCheck, handlePostOrder);
-orderRouter.route('/:id').delete(authenticateToken, adminCheck, handleDeleteOrder);
-
+orderRouter.route('/').post(authenticateToken, handlePostOrder);
+orderRouter
+  .route('/:id')
+  .delete(authenticateToken, adminCheck, handleDeleteOrder);
 
 export default orderRouter;
